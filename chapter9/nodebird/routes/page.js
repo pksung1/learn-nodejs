d54dfbfcd0596dd5e043ee9360e3fd77c4bce101
pksung1 <seonpark@enercamp.com>
2021-10-05 09:51:36 +0900

    <FEATURE> nodebird app

---
+++ chapter9/nodebird/routes/page.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerIdList = [];
+    next();
+})
+
+router.get('/profile', (req, res) => {
+    res.render('profile', {title: 'my info - NodeBird'})
+})
+
+router.get('/join', (req, res) => {
+    res.render('join', {title: 'signup - NodeBird'})
+})
+
+router.get('/', (req, res, next) => {
+    const twits = []
+    res.render('main', {
+        title: 'NodeBird',
+        twits
+    })
+})
+
+module.exports = router
